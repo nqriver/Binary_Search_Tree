2e4005b66b4d6b36578976277ff045e598cfc25f
nqriver <nqriver@gmail.com>
2021-05-15 00:28:48 +0200

    Random tree generation

--- main.cpp
+++ main.cpp
@@ -1,51 +1,19 @@
-#include <iostream>
-#include <fstream>
 #include "BinarySearchTree.h"
-#include <clocale>
-#include <ctime>
-#include <Windows.h>
+#include <fstream>
+#include <random>
+
 
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
-    setvbuf(stdout, nullptr, _IOFBF, 1000);
-    srand(time(nullptr));
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> distrib(1,1000);
 
-
-    std::vector<int> vec { 1,23,4,5 };
-    BinarySearchTree<int> tree(vec.begin(), vec.end());
-    tree.print();
-
-    BinarySearchTree<float> treeFloat;
-    try {
-        treeFloat.maxElement();
-    }
-    catch (const std::exception& exc){
-        std::cerr << exc.what();
+    BinarySearchTree<int> tree;
+    for (auto count { 0 }; count < 300; ++count){
+        tree.insert(distrib(rng));
     }
 
-    for (int k{ 0 }; k < 20; ++k){
-        auto randInt { std::rand()%100 };
-        tree.insert(randInt);
-    }
-
-    tree.traverse([](const auto& elem) { std::cout << elem << '\t';} );
-
-    std::ofstream out{"tree1.txt"};
-    tree.print(out);
-    std::cout << std::endl;
-    tree.print();
-    int val;
-    std::cout << "Enter element to remove:\n";
-    std::cin>>val;
-    tree.remove(val);
-    std::cout << std::endl;
-    tree.print();
-    std::cout << '\n' << tree.maxElement();
-
-    tree.clear();
-    tree.insert(2);
-    tree.print();
-    std::cout << std::boolalpha << tree.empty();
-    std::cout << std::boolalpha << tree.empty();
+    std::ofstream graphics_tree{ "tree.txt" };
+    tree.print(graphics_tree);
     return 0;
 }
